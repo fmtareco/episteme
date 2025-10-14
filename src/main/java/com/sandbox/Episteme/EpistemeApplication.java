ab9d31e0cf95dcc5fe056fc1b08f21ff132de849
FmTareco <31693762+fmtareco@users.noreply.github.com>
2025-10-14 17:29:49 +0100

    initial version from spring initialzr

---
+++ src/main/java/com/sandbox/Episteme/EpistemeApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.Episteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpistemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpistemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandbox/Episteme/EpistemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.Episteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpistemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
